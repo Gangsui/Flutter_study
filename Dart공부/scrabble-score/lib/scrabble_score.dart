cf77a46324465ec383e142abefc53aa04c9c425f
ppkvkr <ppkvkr2@gmail.com>
2023-12-31 10:49:43 -0600

    12/31

--- Dart공부/scrabble-score/lib/scrabble_score.dart
+++ Dart공부/scrabble-score/lib/scrabble_score.dart
@@ -1,12 +1,8 @@
 // Put your code here
 int answer_(String good) {
-  List <String> value1 = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' ];
-  List <String> value2 = ['D', 'G'  ];
-  List <String> value3 = ['B', 'C', 'M', 'P'  ];
-  List <String> value4 = ['F', 'H', 'V', 'W', 'Y' ];
-  List <String> value5 = ['K'];
-  List <String> value8 = ['J', 'X'  ];
-  List <String> value10 = ['Q', 'Z' ];
+  Map <String,int> valueMap = {'A':1,'E':1,'I':1,'O':1,'U':1,'L':1,'N':1,'R':1,'S':1,'T':1,
+                               'D':2, 'G':2,'B':3,'C':3,'M':3,'P':3,'F':4,'H':4,'V':4,'W':4,
+                               'Y':4,'K':5,'J':8,'X':8,'Q':10,'Z':10};
   int answer = 0;
   
 
@@ -14,42 +10,13 @@ int answer_(String good) {
   
   for (int i =0; i< good.length; i++)
   {
-    if (value1.contains(good[i])){
-      answer += 1;
-    }
-    else if (value2.contains(good[i]))
-    {
-      answer += 2;
-      
-    }
-     else if (value3.contains(good[i]))
-    {
-      answer += 3;
-      
-    }
-    else if (value4.contains(good[i]))
-    {
-      answer += 4;
-      
-    }
-     else if (value5.contains(good[i]))
-    {
-      answer += 5;
-      
-    }
-     else if (value8.contains(good[i]))
-    {
-      answer += 8;
-      
-    }
-    else if (value10.contains(good[i]))
-    {
-      answer += 10;
-      
-    }
-    
+    answer += valueMap[good[i].toUpperCase()] ?? 0;
   }
   return answer;
   
+}
+
+void main(){
+  print(answer_('good'));
 }
 
